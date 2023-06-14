3590b41d9ed3893846558753cc1b53fe1b7d7107
Rachel Yang <rachely@canva.com>
2023-06-14 18:25:20 +1000

    RC-v1.0.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,12 @@
 # Changelog
 
+## 2023-06-14
+
+### 🐞 Fixed
+- Replaced legacy CSS and TS tokens with App UI Kit counterparts
+
+### 🗑️ Removed
+- Removed unused legacy token files
+
 ## 2023-06-13
-Initial public release.
+Initial public release
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import * as React from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./app";
 import "@canva/app-ui-kit/styles.css";
-import "styles/base.css";
 
 const root = createRoot(document.getElementById("root")!);
 function render() {
