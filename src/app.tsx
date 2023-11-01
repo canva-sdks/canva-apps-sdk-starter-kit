d938e16551331eae2494f8bd28d72d53878a7f52
Anto Lepejian <anto.l@canva.com>
2023-11-01 12:26:37 +1100

    skeleton for ai demo app

--- src/app.tsx
+++ src/app.tsx
@@ -1,26 +1,115 @@
-import { Button, Rows, Text } from "@canva/app-ui-kit";
-import { addNativeElement } from "@canva/design";
+import { Button, Rows, FileInput, Text } from "@canva/app-ui-kit";
+import { ImageMimeType } from "@canva/asset";
 import * as React from "react";
 import styles from "styles/components.css";
 
+const COLORIZER_URL =
+  "https://lpfsetcdtc.execute-api.us-east-1.amazonaws.com/invocations";
+
+/**
+ * Represents an image that has been colorized.
+ */
+type ColorizedImage = {
+  /** A unique identifier for the image. */
+  id: string;
+
+  /** The URL where the full-sized colorized image can be accessed. */
+  url: string;
+
+  /** The URL where a thumbnail version of the colorized image can be accessed. */
+  thumbnailUrl: string;
+
+  /** The MIME type of the image. */
+  mimeType: ImageMimeType;
+};
+
+/**
+ * Represents a file that has been provided as input.
+ */
+type InputFile = {
+  /** The name of the input file. */
+  name: string;
+
+  /** The Data URI representing the file's data. */
+  dataUri: string;
+};
+
+/**
+ * Converts a File object to a Base64-encoded string.
+ *
+ * @param file - The File object to be converted.
+ * @returns A promise that resolves to a Base64-encoded string.
+ */
+function fileToBase64(file: File): Promise<string> {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+
+    reader.onload = () => resolve(reader.result as string);
+    reader.onerror = () =>
+      reject(new Error(`Unable to read file: ${file.name}`));
+
+    reader.readAsDataURL(file);
+  });
+}
+
 export const App = () => {
-  const onClick = () => {
-    addNativeElement({
-      type: "TEXT",
-      children: ["Hello world!"],
-    });
+  const [file, setFile] = React.useState<InputFile | undefined>();
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [colorizedImageUrl, setColorizedImageUrl] = React.useState<
+    string | undefined
+  >();
+
+  const colorize = async () => {
+    if (!file) {
+      return;
+    }
+    setIsLoading(true);
+
+    // TODO: Make fetch API call to get colorized image
+
+    // TODO: Set colorized image URL
+
+    // TODO: Upload asset to user library
+
+    // TODO: Add to design
+
+    setIsLoading(false);
+  };
+
+  const onDropAcceptedFiles = async ([file]: File[]) => {
+    // TODO: Encode file to base64 and set file state
   };
 
   return (
     <div className={styles.scrollContainer}>
       <Rows spacing="2u">
         <Text>
-          To make changes to this app, edit the{" "}
-          <code className={styles.code}>src/app.tsx</code> file, then close and
-          reopen the app in the editor to preview the changes.
+          Drop your black and white images to bring them to life using our AI
+          Powered Colorizer.
         </Text>
-        <Button variant="primary" onClick={onClick} stretch>
-          Do something cool
+        <FileInput
+          stretchButton
+          accept={["image/png", "image/jpeg", "image/jpg"]}
+          onDropAcceptedFiles={onDropAcceptedFiles}
+          multiple={false}
+        />
+        {file && (
+          <>
+            {/** TODO: Preview selected file and image */}
+          </>
+        )}
+        {colorizedImageUrl && (
+          <>
+            {/** TODO: Preview colorized image */}
+          </>
+        )}
+        <Button
+          variant="primary"
+          onClick={colorize}
+          loading={isLoading}
+          stretch
+        >
+          Colorize
         </Button>
       </Rows>
     </div>
