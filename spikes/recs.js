da3a2f79001cc53deaa7f0f44b1174749e712a48
wolfgang628 <astndvdpllthr@gmail.com>
2024-07-21 03:32:37 -0600

    Recommendation Alg v1

--- spikes/recs.js
+++ spikes/recs.js
@@ -1,11 +1,84 @@
-function findRelativeLuminance(color) {
-    var redSRGB = color.r / 255;
-    var greenSRGB = color.g / 255;
+function withinRange(colorLinear, adjust) {
+    return (colorLinear.r * adjust <= 1 && colorLinear.g * adjust <= 1 && colorLinear.b * adjust <= 1
+        && colorLinear.r * adjust >= 0 && colorLinear.g * adjust >= 0 && colorLinear.b * adjust >= 0);
+}
+function convertRGBToLinearRGB(color) {
+    var redsRGB = color.r / 255;
+    var greensRGB = color.g / 255;
     var blueSRGB = color.b / 255;
-    var red = ((redSRGB <= 0.03928) ? redSRGB / 12.92 : Math.pow(((redSRGB + 0.055) / 1.055), 2.4));
-    var green = ((greenSRGB <= 0.03928) ? greenSRGB / 12.92 : Math.pow(((greenSRGB + 0.055) / 1.055), 2.4));
-    var blue = ((blueSRGB <= 0.03928) ? blueSRGB / 12.92 : Math.pow(((blueSRGB + 0.055) / 1.055), 2.4));
-    var lum = 0.2126 * red + 0.7152 * green + 0.0722 * blue;
+    var redLinear = ((redsRGB <= 0.03928) ? redsRGB / 12.92 : Math.pow(((redsRGB + 0.055) / 1.055), 2.4));
+    var greenLinear = ((greensRGB <= 0.03928) ? greensRGB / 12.92 : Math.pow(((greensRGB + 0.055) / 1.055), 2.4));
+    var blueLinear = ((blueSRGB <= 0.03928) ? blueSRGB / 12.92 : Math.pow(((blueSRGB + 0.055) / 1.055), 2.4));
+    return { r: redLinear, g: greenLinear, b: blueLinear };
+}
+function convertLinearRGBToRGB(colorLinear) {
+    var redsRGB = ((colorLinear.r <= 491 / 161500) ? colorLinear.r * 12.92 : ((Math.pow(colorLinear.r, (1 / 2.4))) * 1.055) - 0.055);
+    var greensRGB = ((colorLinear.g <= 491 / 161500) ? colorLinear.g * 12.92 : ((Math.pow(colorLinear.g, (1 / 2.4))) * 1.055) - 0.055);
+    var blueSRGB = ((colorLinear.b <= 491 / 161500) ? colorLinear.b * 12.92 : ((Math.pow(colorLinear.b, (1 / 2.4))) * 1.055) - 0.055);
+    var red = redsRGB * 255;
+    var green = greensRGB * 255;
+    var blue = blueSRGB * 255;
+    return { r: Math.round(red), g: Math.round(green), b: Math.round(blue) };
+}
+function convertRGBtoHSL(color) {
+    var r = color.r / 255;
+    var g = color.g / 255;
+    var b = color.b / 255;
+    var x_max = Math.max(r, g, b);
+    var x_min = Math.min(r, g, b);
+    var c = x_max - x_min;
+    var l = (x_max + x_min) / 2;
+    var h;
+    var s;
+    if (c == 0) {
+        h = 0;
+    }
+    else if (x_max == r) {
+        h = 60 * (((g - b) / c) % 6);
+    }
+    else if (x_max == g) {
+        h = 60 * (((b - r) / c) + 2);
+    }
+    else {
+        h = 60 * (((r - g) / c) + 4);
+    }
+    if (l == 0 || l == 1) {
+        s = 0;
+    }
+    else {
+        s = (c / (1 - Math.abs((2 * l) - 1)));
+    }
+    return { h: h, s: s, l: l };
+}
+function convertHSLtoRGB(colorHSL) {
+    var c = (1 - Math.abs((2 * colorHSL.l) - 1)) * colorHSL.s;
+    var h_ = colorHSL.h / 60;
+    var x = c * (1 - Math.abs((h_ % 2) - 1));
+    var R1G1B1;
+    if (h_ < 1) {
+        R1G1B1 = { r: c, g: x, b: 0 };
+    }
+    else if (h_ < 2) {
+        R1G1B1 = { r: x, g: c, b: 0 };
+    }
+    else if (h_ < 3) {
+        R1G1B1 = { r: 0, g: c, b: x };
+    }
+    else if (h_ < 4) {
+        R1G1B1 = { r: 0, g: x, b: c };
+    }
+    else if (h_ < 5) {
+        R1G1B1 = { r: x, g: 0, b: c };
+    }
+    else {
+        R1G1B1 = { r: c, g: 0, b: x };
+    }
+    var m = colorHSL.l - (c / 2);
+    return { r: Math.round((R1G1B1.r + m) * 255), g: Math.round((R1G1B1.g + m) * 255), b: Math.round((R1G1B1.b + m) * 255) };
+}
+function findRelativeLuminance(color) {
+    var colorLinear = convertRGBToLinearRGB(color);
+    var lum = 0.2126 * colorLinear.r + 0.7152 * colorLinear.g + 0.0722 * colorLinear.b;
     return lum;
 }
 function findContrastRatio(fg, bg) {
@@ -37,5 +110,77 @@ function runContrastTests(fg, bg) {
     }
     return result;
 }
-console.log(findContrastRatio({ r: 255, g: 255, b: 255 }, { r: 51, g: 35, b: 35 }));
-console.log(runContrastTests({ r: 255, g: 255, b: 255 }, { r: 51, g: 35, b: 35 }));
+function findBetterColor(original, context) {
+    if (findContrastRatio(original, context) >= 7) {
+        return original;
+    }
+    else {
+        var lumOriginal = findRelativeLuminance(original);
+        var lumContext = findRelativeLuminance(context);
+        var numAdjust = ((7 * lumContext) + 0.3) / lumOriginal;
+        var denomAdjust = (lumContext - 0.3) / (7 * lumOriginal); //Must ensure lumOrginal > 0
+        var originalLinear = convertRGBToLinearRGB(original);
+        var adjusted = original;
+        if (lumOriginal * numAdjust > lumContext && withinRange(originalLinear, numAdjust)) {
+            console.log("Case 1");
+            adjusted = convertLinearRGBToRGB({ r: originalLinear.r * numAdjust, g: originalLinear.g * numAdjust, b: originalLinear.b * numAdjust });
+        }
+        else if (lumContext > lumOriginal * denomAdjust && withinRange(originalLinear, denomAdjust)) {
+            console.log("Case 2");
+            adjusted = convertLinearRGBToRGB({ r: originalLinear.r * denomAdjust, g: originalLinear.g * denomAdjust, b: originalLinear.b * denomAdjust });
+        }
+        else {
+            console.log("Failed");
+        }
+        return adjusted;
+    }
+}
+function findBetterColorBackup(original, context) {
+    if (findContrastRatio(original, context) >= 7) {
+        return original;
+    }
+    else {
+        var adjusted = original;
+        var originalHSL = convertRGBtoHSL(original);
+        if (findContrastRatio(convertHSLtoRGB({ h: originalHSL.h, s: originalHSL.s, l: 1 }), context) >= 7) {
+            console.log("Case 1");
+            for (var i = 0.01; i <= 1 - originalHSL.l; i += 0.01) {
+                adjusted = convertHSLtoRGB({ h: originalHSL.h, s: originalHSL.s, l: originalHSL.l + i });
+                if (findContrastRatio(adjusted, context) >= 7) {
+                    break;
+                }
+            }
+            return adjusted;
+        }
+        else if (findContrastRatio(convertHSLtoRGB({ h: originalHSL.h, s: originalHSL.s, l: 0 }), context) >= 7) {
+            console.log("Case 2");
+            for (var i = 0.01; i <= originalHSL.l; i += 0.01) {
+                adjusted = convertHSLtoRGB({ h: originalHSL.h, s: originalHSL.s, l: originalHSL.l - i });
+                if (findContrastRatio(adjusted, context) >= 7) {
+                    break;
+                }
+            }
+            return adjusted;
+        }
+        else {
+            console.log("Failed");
+            return original;
+        }
+    }
+}
+var fg = { r: Math.round(Math.random() * 255), g: Math.round(Math.random() * 255), b: Math.round(Math.random() * 255) };
+var bg = { r: Math.round(Math.random() * 255), g: Math.round(Math.random() * 255), b: Math.round(Math.random() * 255) };
+// const fg: RGB = { r: 16, g: 94, b: 139 }
+// const bg: RGB = { r: 26, g: 65, b: 14 }
+var fgLinear = convertRGBToLinearRGB(fg);
+var fgHSL = convertRGBtoHSL(fg);
+console.log("Original Colors: ", fg, bg);
+console.log("Contrast Ratio:", findContrastRatio(fg, bg));
+// console.log("FG Lum.: ", findRelativeLuminance(fg))
+// console.log("BG Lum.: ", findRelativeLuminance(bg))
+// console.log(runContrastTests(fg, bg))
+// console.log(fgLinear)
+// console.log(fgHSL)
+// console.log(convertHSLtoRGB(fgHSL))
+console.log("Adjusted Colors: ", findBetterColorBackup(fg, bg), bg);
+console.log("Contrast Ratio: ", findContrastRatio(findBetterColorBackup(fg, bg), bg));
--- spikes/recs.ts
+++ spikes/recs.ts
@@ -4,6 +4,12 @@ type RGB = {
     b: number
 }
 
+type HSL = {
+    h: number
+    s: number
+    l: number
+}
+
 type Result = {
     NormalAA: boolean,
     NormalAAA: boolean,
@@ -11,21 +17,109 @@ type Result = {
     LargeAAA: boolean
 }
 
-function findRelativeLuminance(color: RGB) {
-    const redSRGB: number = color.r/255
-    const greenSRGB: number = color.g/255
+function withinRange(colorLinear: RGB, adjust: number) {
+    return (colorLinear.r * adjust <= 1 && colorLinear.g * adjust <= 1 && colorLinear.b * adjust <= 1 
+        && colorLinear.r * adjust >= 0 && colorLinear.g * adjust >= 0 && colorLinear.b * adjust >= 0)
+}
+
+function convertRGBToLinearRGB(color: RGB): RGB {
+    const redsRGB: number = color.r/255
+    const greensRGB: number = color.g/255
     const blueSRGB: number = color.b/255
 
-    const red: number = ((redSRGB <= 0.03928) ? redSRGB/12.92 : ((redSRGB +0.055)/1.055) ** 2.4)
-    const green: number = ((greenSRGB <= 0.03928) ? greenSRGB/12.92 : ((greenSRGB +0.055)/1.055) ** 2.4)
-    const blue: number = ((blueSRGB <= 0.03928) ? blueSRGB/12.92 : ((blueSRGB +0.055)/1.055) ** 2.4)
+    const redLinear: number = ((redsRGB <= 0.03928) ? redsRGB/12.92 : ((redsRGB +0.055)/1.055) ** 2.4)
+    const greenLinear: number = ((greensRGB <= 0.03928) ? greensRGB/12.92 : ((greensRGB +0.055)/1.055) ** 2.4)
+    const blueLinear: number = ((blueSRGB <= 0.03928) ? blueSRGB/12.92 : ((blueSRGB +0.055)/1.055) ** 2.4)
 
-    const lum: number = 0.2126 * red + 0.7152 * green + 0.0722 * blue
+    return { r: redLinear, g: greenLinear, b: blueLinear }
+}
+
+function convertLinearRGBToRGB(colorLinear: RGB): RGB {
+    const redsRGB: number = ((colorLinear.r <= 491 / 161500) ? colorLinear.r * 12.92 : ((colorLinear.r ** (1/2.4)) * 1.055) - 0.055)
+    const greensRGB: number = ((colorLinear.g <= 491 / 161500) ? colorLinear.g * 12.92 : ((colorLinear.g ** (1/2.4)) * 1.055) - 0.055)
+    const blueSRGB: number = ((colorLinear.b <= 491 / 161500) ? colorLinear.b * 12.92 : ((colorLinear.b ** (1/2.4)) * 1.055) - 0.055)
+
+    const red: number = redsRGB * 255
+    const green: number = greensRGB * 255
+    const blue: number = blueSRGB * 255
+
+    return { r: Math.round(red), g: Math.round(green), b: Math.round(blue) }
+}
+
+function convertRGBtoHSL(color: RGB): HSL {
+    const r = color.r / 255
+    const g = color.g / 255
+    const b = color.b / 255
+
+    const x_max: number = Math.max(r, g, b)
+    const x_min: number = Math.min(r, g, b)
+    const c: number = x_max - x_min
+    const l: number = (x_max + x_min) / 2
+    let h: number
+    let s: number
+
+    if (c == 0) {
+        h = 0
+    }
+    else if (x_max == r) {
+        h = 60 * (((g - b) / c) % 6)
+    }
+    else if(x_max == g) {
+        h = 60 * (((b - r) / c) + 2)
+    }
+    else {
+        h = 60 * (((r - g) / c) + 4)
+    }
+
+    if (l == 0 || l == 1) {
+        s = 0
+    }
+    else {
+        s = (c / (1 - Math.abs((2 * l) - 1)))
+    }
+
+    return { h: h, s: s, l: l }
+}
+
+function convertHSLtoRGB(colorHSL: HSL): RGB {
+    const c: number = (1 - Math.abs((2 * colorHSL.l) - 1)) * colorHSL.s
+    const h_: number = colorHSL.h / 60
+    const x: number = c * (1 - Math.abs((h_ % 2) - 1))
+    let R1G1B1: RGB
+
+    if(h_ < 1) {
+        R1G1B1 = { r: c, g: x, b: 0 }
+    }
+    else if(h_ < 2) {
+        R1G1B1 = { r: x, g: c, b: 0 }
+    }
+    else if(h_ < 3) {
+        R1G1B1 = { r: 0, g: c, b: x }
+    }
+    else if(h_ < 4) {
+        R1G1B1 = { r: 0, g: x, b: c }
+    }
+    else if(h_ < 5) {
+        R1G1B1 = { r: x, g: 0, b: c }
+    }
+    else {
+        R1G1B1 = { r: c, g: 0, b: x }
+    }
+
+    const m: number = colorHSL.l - (c / 2)
+    
+    return { r: Math.round((R1G1B1.r + m) * 255), g: Math.round((R1G1B1.g + m) * 255), b: Math.round((R1G1B1.b + m) * 255) }
+}
+
+function findRelativeLuminance(color: RGB) {
+    const colorLinear: RGB = convertRGBToLinearRGB(color)
+
+    const lum: number = 0.2126 * colorLinear.r + 0.7152 * colorLinear.g + 0.0722 * colorLinear.b
 
     return lum
 }
 
-function findContrastRatio(fg: RGB, bg: RGB): number {
+function findContrastRatio(fg: RGB, bg: RGB) {
     const lum1: number = findRelativeLuminance(fg)
     const lum2: number = findRelativeLuminance(bg)
 
@@ -60,7 +154,89 @@ function runContrastTests(fg: RGB, bg: RGB) {
     return result
 }
 
+// function findBetterColor(original: RGB, context: RGB): RGB {
+//     if (findContrastRatio(original, context) >= 7) {
+//         return original
+//     }
+//     else {
+//         let lumOriginal = findRelativeLuminance(original)
+//         let lumContext = findRelativeLuminance(context)
 
+//         const numAdjust = ((7 * lumContext) + 0.3) / lumOriginal
+//         const denomAdjust = (lumContext - 0.3) / (7 * lumOriginal) //Must ensure lumOrginal > 0
 
-console.log(findContrastRatio({ r: 255, g: 255, b: 255 }, { r: 51, g: 35, b: 35 }))
-console.log(runContrastTests({ r: 255, g: 255, b: 255 }, { r: 51, g: 35, b: 35 }))
+//         const originalLinear = convertRGBToLinearRGB(original)
+//         let adjusted = original
+
+//         if (lumOriginal * numAdjust > lumContext && withinRange(originalLinear, numAdjust)) {
+//             console.log("Case 1")
+//             adjusted =  convertLinearRGBToRGB({ r: originalLinear.r * numAdjust, g: originalLinear.g * numAdjust, b: originalLinear.b * numAdjust })
+//         }
+//         else if (lumContext > lumOriginal * denomAdjust && withinRange(originalLinear, denomAdjust)) {
+//             console.log("Case 2")
+//             adjusted =  convertLinearRGBToRGB({ r: originalLinear.r * denomAdjust, g: originalLinear.g * denomAdjust, b: originalLinear.b * denomAdjust })
+//         }
+//         else {
+//         console.log("Failed")
+//         }
+
+//         return adjusted
+//     }
+// }
+
+function findBetterColor(original: RGB, context: RGB): RGB {
+    if (findContrastRatio(original, context) >= 7) {
+        return original
+    }
+    else {
+        let adjusted = original
+        const originalHSL: HSL = convertRGBtoHSL(original)
+        if(findContrastRatio(convertHSLtoRGB({ h: originalHSL.h, s: originalHSL.s, l: 1 }), context) >= 7) {
+            console.log("Case 1")
+            for(let i: number = 0.01; i <= 1 - originalHSL.l; i += 0.01) {
+                adjusted = convertHSLtoRGB({ h: originalHSL.h, s: originalHSL.s, l: originalHSL.l + i })
+                if (findContrastRatio(adjusted, context) >= 7) {
+                    break
+                }
+            }
+
+            return adjusted
+        }
+        else if(findContrastRatio(convertHSLtoRGB({ h: originalHSL.h, s: originalHSL.s, l: 0 }), context) >= 7) {
+            console.log("Case 2")
+            for(let i: number = 0.01; i <= originalHSL.l; i += 0.01) {
+                adjusted = convertHSLtoRGB({ h: originalHSL.h, s: originalHSL.s, l: originalHSL.l - i })
+                if (findContrastRatio(adjusted, context) >= 7) {
+                    break
+                }
+            }
+
+            return adjusted
+        }
+        else {
+            console.log("Failed")
+            return original
+        }
+    }
+}
+
+const fg: RGB = { r: Math.round(Math.random() * 255), g: Math.round(Math.random() * 255), b: Math.round(Math.random() * 255) }
+const bg: RGB = { r: Math.round(Math.random() * 255), g: Math.round(Math.random() * 255), b: Math.round(Math.random() * 255) }
+// const fg: RGB = { r: 16, g: 94, b: 139 }
+// const bg: RGB = { r: 26, g: 65, b: 14 }
+
+const fgLinear: RGB = convertRGBToLinearRGB(fg)
+const fgHSL: HSL = convertRGBtoHSL(fg)
+
+console.log("Original Colors: ", fg, bg)
+console.log("Contrast Ratio:", findContrastRatio(fg, bg))
+// console.log("FG Lum.: ", findRelativeLuminance(fg))
+// console.log("BG Lum.: ", findRelativeLuminance(bg))
+// console.log(runContrastTests(fg, bg))
+
+// console.log(fgLinear)
+// console.log(fgHSL)
+// console.log(convertHSLtoRGB(fgHSL))
+
+console.log("Adjusted Colors: ", findBetterColor(fg, bg), bg)
+console.log("Contrast Ratio: ", findContrastRatio(findBetterColor(fg, bg), bg))
