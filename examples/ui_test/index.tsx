c998f5e1b36899cc132f8db451504c2796e00ef0
Matt Bowolick <mbowolick@canva.com>
2024-07-24 11:17:01 +1000

    2024-07-24

---
+++ examples/ui_test/index.tsx
@@ -0,0 +1,28 @@
+import { createRoot } from "react-dom/client";
+import { App } from "./app";
+import "@canva/app-ui-kit/styles.css";
+import { AppUiProvider } from "@canva/app-ui-kit";
+import { addNativeElement } from "@canva/design";
+
+const root = createRoot(document.getElementById("root") as Element);
+function render() {
+  root.render(
+    <AppUiProvider>
+      {/* Any Apps SDK method needs to be injected to the component, to avoid the need to mock it in tests */}
+      <App
+        onClick={() =>
+          addNativeElement({
+            type: "TEXT",
+            children: ["Hello world!"],
+          })
+        }
+      />
+    </AppUiProvider>
+  );
+}
+
+render();
+
+if (module.hot) {
+  module.hot.accept("./app", render);
+}
--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,16 @@
+const { pathsToModuleNameMapper } = require('ts-jest')
+const { compilerOptions } = require('./tsconfig.json')
+
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   testRegex: '(/tests/.*|(\\.|/)(tests))\\.tsx?$',
-  modulePathIgnorePatterns: ['./internal/'],
-  testPathIgnorePatterns: [ '/node_modules/', '/dist/']
+  modulePaths: [compilerOptions.baseUrl],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
+  transform: {
+    ".+\\.(css)$": "<rootDir>/node_modules/jest-css-modules-transform",
+  },
+  transformIgnorePatterns: ['node_modules'],
+  testPathIgnorePatterns: [ '/node_modules/', '/dist/'],
 };
