5edb359a64f37914f0f020a58066dc72823c5d24
dram0027/usr/local/bin/python3 <dram0027@student.monash.edu>
2024-07-31 01:32:03 +1000

    Front-end for showcase component

---
+++ src/ShowcaseComponent.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "styles/components.css";
+
+
+export const ShowcaseComponent = () => (
+  <div className={styles.bgContainer}>
+    <h3 className={styles.fgText}>
+        Aa
+    </h3>
+  </div>
+);
