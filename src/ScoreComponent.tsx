32d006758e46b4cabd31a0e1fb32ea9ecc2c6330
Diya <dram0027@student.monash.edu>
2024-07-31 22:46:23 +1000

    Calculating contrast score and rendering onto DOM

--- src/ScoreComponent.tsx
+++ src/ScoreComponent.tsx
@@ -1,60 +1,68 @@
 import { Box, Columns, Column, Rows, Text, Placeholder, HelpCircleIcon, Button } from "@canva/app-ui-kit";
 import React from "react";
-import styles from "styles/components.css";
 import { ShowcaseComponent } from "./ShowcaseComponent";
 import { Pill } from "./Pill";
+import { calculateContrast } from "./utils";
 
-export const ScoreComponent = () => (
-  <div>
-    <Rows spacing="2u">
-      <Columns spacing="2u" alignY="center">
-        <Column width="content">
-          {/* <Placeholder shape="square"></Placeholder> */}
-          <ShowcaseComponent></ShowcaseComponent>
-        </Column>
-        <Column>
-            <Text variant="bold">000.00</Text>
-        </Column>
-        <Column>
-          <HelpCircleIcon></HelpCircleIcon>
-        </Column>
-      </Columns>
+type ScoreComponentProps = {
+  fgColour: string;
+  bgColour: string;
+};
 
-      <Columns spacing="2u" alignY="center">
-        <Column width="1/4">
-          <Text variant="bold">Large</Text>
-        </Column>
-        <Column width="content">
-          <Text>AA</Text>
-        </Column>
-        <Column width="content">
-          <Pill variant="Pass">Pass</Pill>
-        </Column>
-        <Column width="content">
-          <Text>AAA</Text>
-        </Column>
-        <Column width="content">
-          <Pill variant="Fail">Pass</Pill>
-        </Column>
-      </Columns>
+export const ScoreComponent: React.FC<ScoreComponentProps> = ({ fgColour, bgColour }) => {
+  const contrastScore = calculateContrast(fgColour, bgColour);
 
-      <Columns spacing="2u" alignY="center">
-        <Column width="1/4">
-          <Text variant="bold">Normal</Text>
-        </Column>
-        <Column width="content">
-              <Text>AA</Text>
-            </Column>
-            <Column width="content">
-              <Pill variant="Fail">Pass</Pill>
-            </Column>
-            <Column width="content">
-              <Text>AAA</Text>
-            </Column>
-            <Column width="content">
-              <Pill variant="Fail">Pass</Pill>
-            </Column>
-      </Columns>
-    </Rows>
-  </div>
-);
+  return (
+    <div>
+      <Rows spacing="2u">
+        <Columns spacing="2u" alignY="center">
+          <Column width="content">
+            <ShowcaseComponent fgColour={fgColour} bgColour={bgColour} />
+          </Column>
+          <Column>
+            <Text variant="bold">{contrastScore.toFixed(2)}</Text>
+          </Column>
+          <Column>
+            <HelpCircleIcon />
+          </Column>
+        </Columns>
+
+        <Columns spacing="2u" alignY="center">
+          <Column width="1/4">
+            <Text variant="bold">Large</Text>
+          </Column>
+          <Column width="content">
+            <Text>AA</Text>
+          </Column>
+          <Column width="content">
+            <Pill variant={contrastScore >= 3 ? "Pass" : "Fail"}>{contrastScore >= 3 ? "Pass" : "Fail"}</Pill>
+          </Column>
+          <Column width="content">
+            <Text>AAA</Text>
+          </Column>
+          <Column width="content">
+            <Pill variant={contrastScore >= 4.5 ? "Pass" : "Fail"}>{contrastScore >= 3 ? "Pass" : "Fail"}</Pill>
+          </Column>
+        </Columns>
+
+        <Columns spacing="2u" alignY="center">
+          <Column width="1/4">
+            <Text variant="bold">Normal</Text>
+          </Column>
+          <Column width="content">
+            <Text>AA</Text>
+          </Column>
+          <Column width="content">
+            <Pill variant={contrastScore >= 4.5 ? "Pass" : "Fail"}>{contrastScore >= 4.5 ? "Pass" : "Fail"}</Pill>
+          </Column>
+          <Column width="content">
+            <Text>AAA</Text>
+          </Column>
+          <Column width="content">
+            <Pill variant={contrastScore >= 7 ? "Pass" : "Fail"}>{contrastScore >= 7 ? "Pass" : "Fail"}</Pill>
+          </Column>
+        </Columns>
+      </Rows>
+    </div>
+  );
+};
---
+++ src/utils.ts
@@ -0,0 +1,44 @@
+type linearRGB = {
+    r: number
+    g: number
+    b: number
+}
+
+export const calculateContrast = (fgColour: string, bgColour: string): number => {
+    
+    const hexToRGB = (hex: string): linearRGB => {
+
+        // convering hex to RGB
+        const bigint = parseInt(hex.substring(1), 16);
+        const redsRGB = ((bigint >> 16) & 255)/255;
+        const greensRGB = ((bigint >> 8) & 255)/255;
+        const blueSRGB = (bigint & 255)/255;
+        
+        // converting to linear RGB
+        const redLinear: number = ((redsRGB <= 0.03928) ? redsRGB/12.92 : ((redsRGB +0.055)/1.055) ** 2.4)
+        const greenLinear: number = ((greensRGB <= 0.03928) ? greensRGB/12.92 : ((greensRGB +0.055)/1.055) ** 2.4)
+        const blueLinear: number = ((blueSRGB <= 0.03928) ? blueSRGB/12.92 : ((blueSRGB +0.055)/1.055) ** 2.4)
+        
+        return { r: redLinear, g: greenLinear, b: blueLinear };
+    };
+
+    const findRelativeLuminance = (colour: linearRGB): number => {
+        const lum: number = 0.2126 * colour.r + 0.7152 * colour.g + 0.0722 * colour.b
+    
+        return lum
+    }
+    
+    const fgLinearRGB = hexToRGB(fgColour);
+    const bgLinearRGB = hexToRGB(bgColour);
+    const lumFG = findRelativeLuminance(fgLinearRGB);
+    const lumBG = findRelativeLuminance(bgLinearRGB);
+    
+    let contrastRatio: number = (lumFG + 0.05) / (lumBG + 0.05)
+    // Contrast ratio must always be >= 1
+    if (contrastRatio < 1) {
+        contrastRatio = 1/contrastRatio
+    }
+
+    return contrastRatio;
+  };
+  
