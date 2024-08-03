d93284a84f3e55dd2e810685e09218dc71ab8d77
bdan0005 <bdan0005@student.monash.edu>
2024-08-03 23:16:36 +1000

    Implemented reverse colour button functionality

--- src/ChooseColours.tsx
+++ src/ChooseColours.tsx
@@ -1,5 +1,7 @@
-import { Rows, Columns, Column, ColorSelector, Text, Title, Button, RotateIcon } from "@canva/app-ui-kit";
+import { Rows, Columns, Column, ColorSelector, Text, Title, Button, RotateIcon, Swatch } from "@canva/app-ui-kit";
 import React from "react";
+import { useState } from "react";
+import { openColorSelector } from "@canva/asset";
 
 type ChooseColoursProps = {
   fgColour: string;
@@ -10,6 +12,15 @@ type ChooseColoursProps = {
 };
 
 export const ChooseColours: React.FC<ChooseColoursProps> = ({ fgColour, bgColour, onChangeFgColour, onChangeBgColour, onClick }) => {
+  const [fgFill, setFgFill] = useState(fgColour);
+  const [bgFill, setBgFill] = useState(bgColour);
+  
+  const reverseColors = (): void => {
+    const prevFgFill = fgFill;
+    setFgFill(bgFill);
+    setBgFill(prevFgFill);
+  };
+
   return (
       <Rows spacing="0">
         <Title size="medium">Choose colours</Title>
@@ -18,12 +29,12 @@ export const ChooseColours: React.FC<ChooseColoursProps> = ({ fgColour, bgColour
             <Text size="medium">Foreground</Text>
           </Column>
           <Column width="content">
-            <ColorSelector color={fgColour} onChange={onChangeFgColour}></ColorSelector>
+            <ColorSelector color={fgFill} onChange={onChangeFgColour}></ColorSelector>
           </Column>
         </Columns>
         <Columns spacing="0.5u" align="end">
           <Column>
-            <Button variant="tertiary" icon={RotateIcon}></Button>
+            <Button variant="tertiary" icon={RotateIcon} onClick={reverseColors}></Button>
           </Column>
         </Columns>
         <Columns spacing="0.5u" alignY="center">
@@ -31,7 +42,7 @@ export const ChooseColours: React.FC<ChooseColoursProps> = ({ fgColour, bgColour
             <Text size="medium">Background</Text>
           </Column>
           <Column width="content">
-            <ColorSelector color={bgColour} onChange={onChangeBgColour}></ColorSelector>
+            <ColorSelector color={bgFill} onChange={onChangeBgColour}></ColorSelector>
           </Column>
         </Columns>
       </Rows>
